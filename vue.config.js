d7027a1e06e7c37769f4106ef564608255bb7f7f
硫酸 <lsch001@163.com>
2019-10-30 18:33:04 +0800

    :wrench: Config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,23 @@
-// const path = require('path')
-// const resolve = dir => {
-//     return path.join(__dirname, dir)
-// }
+const path = require('path')
+const resolve = dir => {
+    return path.join(__dirname, dir)
+}
 // module.exports = {
 //     chainWebpack: config => {
 //         config.resolve.alias
 //             .set('@$', resolve('src'))
 //     }
 // }
+
+module.exports = {
+    publicPath: './',
+    chainWebpack: config => {
+        config
+            .module
+            .rule("glsl")
+            .test(/\.(glsl|frag|vert)$/)
+            .use("raw-loader")
+            .loader("raw-loader")
+            .end();
+    }
+}
