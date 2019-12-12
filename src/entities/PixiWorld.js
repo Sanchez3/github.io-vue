822a979b29177d0256104e72865cf64229c87a8b
硫酸 <lsch001@163.com>
2019-12-12 18:30:40 +0800

    :construction: Building

--- src/entities/PixiWorld.js
+++ src/entities/PixiWorld.js
@@ -8,6 +8,22 @@ import { gsap } from "gsap";
 import Tendril from './Tendrils.js'
 class PixiWorld {
     constructor() {
+        if (!String.prototype.padStart) {
+            String.prototype.padStart = function padStart(targetLength, padString) {
+                targetLength = targetLength >> 0; //floor if number or convert non-number to 0;
+                padString = String((typeof padString !== 'undefined' ? padString : ' '));
+                if (this.length > targetLength) {
+                    return String(this);
+                } else {
+                    targetLength = targetLength - this.length;
+                    if (targetLength > padString.length) {
+                        padString += padString.repeat(targetLength / padString.length); //append to original to ensure we are longer than needed
+                    }
+                    return padString.slice(0, targetLength) + String(this);
+                }
+            };
+        }
+
         this.init()
     }
     initSprite(res, no) {
@@ -22,7 +38,6 @@ class PixiWorld {
             var frames = _res[_no].data.frames;
             var meta = _res[_no].data.meta;
             var textures = [];
-            console.log(frames)
             var count = 0;
             for (var f in frames) {
                 var texture = PIXI.Texture.from(f)
@@ -52,6 +67,7 @@ class PixiWorld {
             sprite.scale.set(1);
             sprite.canplay = false;
         }
+        sprite.alpha = 0;
 
 
         return sprite
@@ -96,34 +112,39 @@ class PixiWorld {
         app.loader.load(onLoaded.bind(this))
 
         function onLoaded(loader, resources) {
+            var glitchfilter = new GlitchFilter({ slices: 1, offset: 0, direction: 90, fillMode: 0, seed: 0.5, red: [0, 0], blue: [0, 0] })
+            // this.pjs.filters = [glitchfilter]
             var pjNo = 0;
             this.pjs = new PIXI.Container();
+            this.pjs.width = app.screen.width * 2;
+            this.pjs.height = app.screen.height * 2;
             app.stage.addChild(this.pjs)
             for (let i = 0; i < 11; i++) {
                 let no = i < 10 ? '0' + i : i;
                 this.pic = this.initSprite(resources, no);
                 this.pic.x = app.screen.width / 2;
                 this.pic.y = app.screen.height / 2;
+
                 this.pjs.addChild(this.pic)
+
             }
-
-
+            this.pjs.getChildAt(0).alpha = 1;
+            app.stage.filters = [glitchfilter]
 
             // this.picT = resources.pic1.texture;
             app.stage.interactive = true;
             app.stage.on('pointerdown', function() {
                 // console.log(tendril.dots)
             })
-            var glitchfilter = new GlitchFilter({ slices: 0, offset: 0, direction: 90, fillMode: 0, seed: 0.5, red: [0, 0], blue: [0, 0] })
-            this.pic.filters = [glitchfilter]
-            glitchfilter.animating = false;
 
+            glitchfilter.animating = false;
+            console.log(glitchfilter)
             var gui = new dat.GUI({ autoPlace: false });
             document.getElementsByClassName('gui-container')[0].appendChild(gui.domElement);
             gui.add(glitchfilter, 'animating').name('(animating)');
             gui.add(glitchfilter, 'seed', 0, 1).step(0.01);
-            gui.add(glitchfilter, 'slices').min(0).max(20).step(1).name('slices');
-            gui.add(glitchfilter, 'offset', -400, 1000);
+            gui.add(glitchfilter, 'slices').min(0).max(20).step(1).name('slices').listen();
+            gui.add(glitchfilter, 'offset', -400, 1000).listen();;
             gui.add(glitchfilter, "direction", -180, 180);
             gui.add(glitchfilter, "fillMode", {
                 TRANSPARENT: 0,
@@ -148,17 +169,19 @@ class PixiWorld {
             var tendril = new Tendril({ spring: 0.5 })
             tendril.target = { x: Math.random() * app.screen.width, y: Math.random() * app.screen.height }
             var _pjNo = pjNo < 10 ? '0' + pjNo : pjNo;
-            console.log((''+pjNo).padStart(2,'0'))
-            var vertices = resources[`v${(''+pjNo).padStart(2,'0')}`].data[`${(''+pjNo).padStart(2,'0')}`].vertices;
-            var verticesL = vertices.length;
+            console.log(('' + pjNo).padStart(2, '0'))
+            // var vertices = resources[`v${(''+pjNo).padStart(2,'0')}`].data[`${(''+pjNo).padStart(2,'0')}`].vertices;
+            // var verticesL = vertices.length;
 
             function ttoV(t) {
+                var vertices = resources[`v${(''+pjNo).padStart(2,'0')}`].data[`${(''+pjNo).padStart(2,'0')}`].vertices;
+                var verticesL = vertices.length;
                 if (vDelta >= verticesL) vDelta = 0;
                 if (vDelta % 1 === 0) {
 
                     t.x = vertices[vDelta].x + this.pjs.getChildAt(pjNo).x;
                     t.y = vertices[vDelta].y + this.pjs.getChildAt(pjNo).y;
-                    console.log(t.x, t.y)
+                    // console.log(t.x, t.y)
                 }
 
 
@@ -174,43 +197,47 @@ class PixiWorld {
                 startTime = Date.now();
                 // tendril.target = { x: Math.random() * app.screen.width, y: Math.random() * app.screen.height }
             }, this)
+            var _sliceDelta = 0;
             this.pic.on('pointerup', function() {
                 glitchfilter.animating = false;
                 pressing = false
                 gsap.set(glitchfilter, { red: [0, 0], green: [0, 0], blue: [0, 0] })
-                gsap.to(glitchfilter, 0.1, {
+                gsap.to(glitchfilter, 0.2, {
                     offset: 0,
-                    slices: 0,
-                    onUpdate: function() {
+                    onComplete: function() {
+                        glitchfilter.slices = 0;
+                        _sliceDelta = 0;
                         // console.log(glitchfilter.green)
                     }
                 })
-
-                var elapsed = Date.now() - startTime;
-                if (elapsed > 1000) {
-                    pjNo++;
-                }
-
-            })
+            }, this)
             this.pic.on('pointerover', function() {
-                if (this.pic.canplay)
-                    this.pic.play()
+                if (this.pjs.getChildAt(pjNo).canplay)
+                    this.pjs.getChildAt(pjNo).play();
             }, this)
             this.pic.on('pointerout', function() {
-                if (this.pic.canplay)
-                    this.pic.stop()
+                if (this.pjs.getChildAt(pjNo).canplay)
+                    this.pjs.getChildAt(pjNo).stop();
             }, this)
+            var elapsed = 0;
 
             this.events.on('animate', function() {
                 // glitchfilter.animating && (glitchfilter.seed = Math.random())
                 if (glitchfilter.animating) {
-                    glitchfilter.offset = Math.random() * 50 - 50;
-                    glitchfilter.slices = Math.random() * 10
+                    this.events.emit('glitch');
+                    // glitchfilter.offset += Math.random() * 50;
+                    // glitchfilter.slices++;
                     glitchfilter.red = [Math.random() * 5 - 10, Math.random() * 5 - 10]
                     glitchfilter.green = [Math.random() * 5 - 10, Math.random() * 5 - 10]
                     glitchfilter.blue = [Math.random() * 5 - 10, Math.random() * 5 - 10]
                     ttoV.bind(this, tendril.target)()
+                    elapsed = Date.now() - startTime;
+                    if (elapsed > 2000) {
+                        startTime = Date.now();
+                        this.events.emit('change');
+                    }
                 }
+
                 tendril.update();
                 tendril.draw(graphics);
             }, this)
@@ -233,7 +260,7 @@ class PixiWorld {
                     tendril.target.x = ex;
                     tendril.target.y = ey;
                 }
-                console.log(ex, ey)
+                // console.log(ex, ey)
                 if (ex > 0.55 * iWidth) ex = 0.55 * iWidth;
                 if (ex < 0.45 * iWidth) ex = 0.45 * iWidth;
                 if (ey > 0.55 * iHeight) ey = 0.55 * iHeight;
@@ -243,19 +270,52 @@ class PixiWorld {
             }
             this.events.on('parallax', function() {
                 if (mouseY === null || mouseX === null) return
-                this.pic.x += (mouseX - this.pic.x) * 0.008;
-                this.pic.y += (-mouseY - this.pic.y) * 0.008;
+                this.pjs.getChildAt(pjNo).x += (mouseX - this.pjs.getChildAt(pjNo).x) * 0.008;
+                this.pjs.getChildAt(pjNo).y += (-mouseY - this.pjs.getChildAt(pjNo).y) * 0.008;
             }, this);
-            // this.events.on('pressing', function() {
-            // }, this)
 
+            this.events.on('glitch', function() {
+                if (glitchfilter.offset < app.screen.height / 2)
+                    glitchfilter.offset += Math.random() * 10;
+                if (_sliceDelta < 50) {
+                    _sliceDelta += 0.5;
+                }
+                glitchfilter.seed = Math.random()
+
+                console.log(_sliceDelta)
+                glitchfilter.slices = Math.floor(_sliceDelta);
+                // if (_sliceDelta % 1 === 0){
+                //     console.log(_sliceDelta)
+                //     glitchfilter.slices = (_sliceDelta % 1);
+                // }
+            }, this)
+            function renderPj(pjNo){
+
+            }
+
+            this.events.on('change', function() {
+                var t0=gsap.to(this.pjs.getChildAt(pjNo), 0.3, { alpha: 0, paused: false })
+                // .alpha = 0;
+                if (this.pjs.getChildAt(pjNo).canplay)
+                    this.pjs.getChildAt(pjNo).stop();
+                pjNo++;
+                pjNo = pjNo > 10 ? 0 : pjNo;
+                var pjNo_n=pjNo;
+                var t1=gsap.to(this.pjs.getChildAt(pjNo_n), 0.3, { alpha: 1, paused: false })
+                // this.pjs.getChildAt(pjNo).alpha = 1;
+                if (this.pjs.getChildAt(pjNo).canplay) this.pjs.getChildAt(pjNo).play();
+
+
+
+            }, this)
+            // this.events.emit('change');
         }
     }
     animate(t) {
         this.animateTimer += t;
         var n = this.animateTimer;
         this.events.emit('animate', t, n);
-        // this.events.emit('pressing');
+
         this.events.emit('parallax');
 
     }
